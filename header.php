066e94ebfbf75d647a642f27c4080289955aa65b
KluskaGit <kamil6kluska7@gmail.com>
2022-11-29 20:41:38 +0100

    add header

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="style.css">
+ <title>Jomix</title>
+  </head>
+  <body>
+    <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><img src="jomix-300.png" width="100" height="50"/></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+      
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      
+      <div id="naglowek">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="me-auto"></div> 
+           <ul class="navbar-nav">
+            <li class="nav-item" >
+              <a class="nav-link" href="Produkty/Produkty.php">SUKIENKI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Produkty/Produkty.php">BLUZKI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Produkty/Produkty.php">BLUZY,KARDIGANY</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Produkty/Produkty.php">SPODNIE</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Produkty/Produkty.php">SPÓDNICE</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Produkty/Produkty.php">KOMPLETY</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Koszyk/Koszyk.php"><img src="cart.png" width="30" height="30"></a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+    </nav>
