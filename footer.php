fd5a494db5babb0989c1e0f6cf98304a4144a184
FiodorowPhotography <fiodo037@gmail.com>
2022-11-29 22:03:15 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,8 +1,26 @@
-<footer class="row row-cols-5 py-5 my-5 border-top">
-          <div class="col">
-      
-          </div>
-      
+<style>
+  ul
+  {
+    text-align:center;
+  }
+  .foocik
+  {
+    background-color:gainsboro;
+    height:50px;
+    width:100%;
+  }
+  .row
+  {
+    display:flex;
+    justify-content:center;
+    align-items:center
+  
+  }
+
+</style>
+
+<footer>
+        <div class="row row-cols-3 py-3 my-3">
           <div class="col">
             <h4>Konto</h4>
             <ul class="nav flex-column">
@@ -10,17 +28,6 @@
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Zarejestruj się</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Zamówienia</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Koszyk</a></li>
-            </ul>
-          </div>
-      
-          <div class="col">
-            <h4>Informacje</h4>
-            <ul class="nav flex-column">
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Regulamin sklepu</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Warunki dostawy</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Polityka zwrotów</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Polityka prywatności</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kontakt</a></li>
             </ul>
           </div>
       
@@ -33,5 +40,21 @@
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Spódnice</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Spodnie</a></li>
             </ul>
+          </div>
+
+          <div class="row">
+            <ul class="nav flex-row">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><img src="zdjecia/instagram.png" width=28px height=28px></a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><img src="zdjecia/facebook.png" width=28px height=28px></a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><img src="zdjecia/telephone.png" width=28px height=28px></a></li>
+            </ul>
+          </div>
+
+
+
+        </div>
+
+          <div class="foocik">
+              <p>© 2022 Jomix - sklep z odzieżą damską.</p>
           </div>
         </footer>
