38793654bba761bd26b91a4b7d54b767dff247a9
KluskaGit <kamil6kluska7@gmail.com>
2022-11-30 11:36:06 +0100

    user_admin_database

---
+++ FiodorowKluskaDatabase.sql
@@ -0,0 +1,129 @@
+DROP DATABASE If EXISTS jomix;
+CREATE DATABASE jomix;
+use jomix;
+
+
+
+DROP TABLE IF EXISTS uzytkownicy;
+CREATE TABLE uzytkownicy
+(
+    userID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    imie varchar(55),
+    nazwisko varchar(55),
+    login varchar(55),
+    haslo varchar(256),
+    admin tinyint(1),
+    email varchar(55),
+    nr_tel varchar(9),
+    kraj varchar(55),
+    kod_pocztowy varchar(6),
+    poczta varchar(55),
+    adres1 varchar(55),
+    adres2 varchar(55),
+    koszykID int(11)
+);
+
+DROP TABLE IF EXISTS koszyk;
+CREATE TABLE koszyk
+(
+    koszykID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userID int(11) NOT NULL,
+    produktID int(11) NOT NULL,
+    ilosc int NOT NULL DEFAULT 1,
+    cena float(11) NOT NULL
+  
+);
+
+ALTER TABLE uzytkownicy
+ADD CONSTRAINT FOREIGN KEY (koszykID) REFERENCES koszyk (koszykID);
+
+
+DROP TABLE IF EXISTS zamowienia;
+CREATE TABLE zamowienia
+(
+    zamowienieID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userID int(11) NOT NULL,
+    status_zamowienia varchar(55) NOT NULL,
+    kwota_zamowienia float(11) NOT NULL,
+    platnoscID int(11) NOT NULL,
+    dostawaID int(11) NOT NULL,
+    data_zamowienia DATETIME,
+    email varchar(55),
+    nr_tel varchar(9),
+    kraj varchar(55),
+    kod_pocztowy varchar(6),
+    poczta varchar(55),
+    adres1 varchar(55),
+    adres2 varchar(55)
+    
+);
+
+ALTER TABLE zamowienia
+ADD CONSTRAINT FOREIGN KEY (userID) REFERENCES uzytkownicy (userID);
+
+DROP TABLE IF EXISTS szczegoly_zamowienia;
+CREATE TABLE szczegoly_zamowienia
+(
+    szczegoly_zamowieniaID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    zamowienieID int(11) NOT NULL,
+    produktID int(11) NOT NULL,
+    ilosc int NOT NULL DEFAULT 1,
+    cena float(11)
+);
+
+ALTER TABLE szczegoly_zamowienia
+ADD CONSTRAINT FOREIGN KEY (zamowienieID) REFERENCES zamowienia (zamowienieID);
+
+DROP TABLE IF EXISTS produkty;
+CREATE TABLE produkty
+(
+    produktID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userID int(11),
+    kategoriaID int(11) NOT NULL,
+    nazwa_produktu varchar(55) NOT NULL,
+    opis_produktu varchar(300),
+    ilosc int(11) NOT NULL,
+    cena float(6) NOT NULL,
+    promocja float NOT NULL DEFAULT 0,
+    SKU varchar(100) NOT NULL,
+    img_url varchar(256)
+);
+
+ALTER TABLE produkty
+ADD CONSTRAINT FOREIGN KEY (userID) REFERENCES uzytkownicy (userID);
+
+
+
+DROP TABLE IF EXISTS kategorie;
+CREATE TABLE kategorie
+(
+    kategoriaID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nazwa_kategorii varchar(55) NOT NULL
+);
+
+
+ALTER TABLE produkty
+ADD CONSTRAINT FOREIGN KEY (kategoriaID) REFERENCES kategorie (kategoriaID);
+
+DROP TABLE IF EXISTS platnosc;
+CREATE TABLE platnosc
+(
+    platnoscID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    metoda_platnosci int(11) NOT NULL
+);
+
+ALTER TABLE zamowienia
+ADD CONSTRAINT FOREIGN KEY (platnoscID) REFERENCES platnosc (platnoscID);
+
+
+
+DROP TABLE IF EXISTS Dostawcy;
+CREATE TABLE Dostawcy
+(
+    dostawcyID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nazwa_dostawcy varchar(55) NOT NULL,
+    cena_dostawy float(11) NOT NULL
+);
+
+ALTER TABLE zamowienia
+ADD CONSTRAINT FOREIGN KEY (dostawaID) REFERENCES Dostawcy (dostawcyID);
--- user.php
+++ user.php
@@ -1,12 +1,40 @@
 <?php
     include("header.php");
 ?>
+
+<?php
+  $admin=1;
+  $wyswietl_admin="";
+
+  if($admin==0)
+  {
+    $wyswietl_admin="none";
+  }
+  else
+  {
+    $wyswietl_admin="flex";
+  }
+
+?>
+<style>
+  #list-admin-list
+  {
+    display: <?php echo "$wyswietl_admin" ?>;
+    border-bottom: none;
+    
+  }
+  #list-kokpit-list
+  {
+    border-top: 1px solid rgba(0,0,0,.125);
+  }
+</style>
 <div class="container">
     <h2 class="user_tk">Twoje Konto</h2>
 
     <div class="row">
       <div class="col-4">
         <div class="list-group" id="list-tab" role="tablist">
+          <a class="list-group-item list-group-item-action" id="list-admin-list" data-bs-toggle="list" href="#list-admin" role="tab" aria-controls="list-admin">Admin</a>
           <a class="list-group-item list-group-item-action active" id="list-kokpit-list" data-bs-toggle="list" href="#list-kokpit" role="tab" aria-controls="list-kokpit">Kokpit</a>
           <a class="list-group-item list-group-item-action" id="list-zamowienia-list" data-bs-toggle="list" href="#list-zamowienia" role="tab" aria-controls="list-zamowienia">Zamówienia</a>
           <a class="list-group-item list-group-item-action" id="list-dane-list" data-bs-toggle="list" href="#list-dane" role="tab" aria-controls="list-dane">Twoje Dane</a>
@@ -16,19 +44,14 @@
       </div>
       <div class="col-8">
         <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show" id="list-admin" role="tabpanel" aria-labelledby="list-admin-list">admin</div>
             <div class="tab-pane fade show active" id="list-kokpit" role="tabpanel" aria-labelledby="list-kokpit-list">
                 <div class="kokpit">
                     <a>Tutaj, możesz zarządzać swoimi zamówieniami, zwrotami oraz ustawieniami konta.</a>
                     <br>
                     <br>
                     <h3>Ostatnie Zamówienie</h3>
-                    <div class="tescik t2">
-                      adadad
-                    </div>
-
-                    <div class="tescik">
-                      swwswsw
-                    </div>
+                    
                     
                 </div>
             </div>
