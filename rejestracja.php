18acf76c659cfb19ebd3015de223b64dc56d1a5a
KluskaGit <kamil6kluska7@gmail.com>
2022-12-01 23:35:01 +0100

    rejestracja_ok

--- rejestracja.php
+++ rejestracja.php
@@ -1,56 +1,68 @@
-<?php
-include 'head.php';
-/*
-$IMIE = "";
-$NAZWISKO = "";
-$TYTUL = "";
-$CENA = "";
+<!DOCTYPE html>
+<html lang="pl-PL">
 
-$host = 'localhost';
-$dbname = 'ksiegarnia1';
-$user = 'user2';
-$pass = 'ZAQ!2wsx';
-include("laczenie.php");
+<head>
+    <meta charset="UTF-8">
+    <?php include 'head.php';
+    ?>
+    <title>Zarejestruj się</title>
+</head>
 
-if (isset($_POST['addButton'])) {
-    $IMIE = $_POST["IMIE"];
-    $NAZWISKO = $_POST["NAZWISKO"];
-    $TYTUL = $_POST["TYTUL"];
-    $CENA = $_POST["CENA"];
+<body>
 
-    $sql = "INSERT INTO ksiazki (imieautora,nazwiskoautora,TYTUL,CENA) 
-            VALUES ('$IMIE','$NAZWISKO','$TYTUL','$CENA')";
-    $stmt = $pdo->exec($sql);
-}
-*/
-?>
+    <?php
+
+    include 'laczenieBaza.php';
+
+    $imie = "";
+    $nazwisko = "";
+    $login = "";
+    $haslo = "";
+    $email = "";
 
 
+    if (isset($_POST['zarejestrujsie'])) {
+        $imie = $_POST["imie"];
+        $nazwisko = $_POST["nazwisko"];
+        $email = $_POST["email"];
+        $haslo = password_hash($_POST["haslo"], PASSWORD_ARGON2I);
+
+        mysqli_query($lacz, "INSERT INTO uzytkownicy (imie,nazwisko,login,haslo,email) 
+    VALUES ('$imie','$nazwisko','$email','$haslo','$email')");
+    }
+
+    ?>
 
 
-<div class="container-lg centruj rejestracjamain">
-    <div class="logowanielogo rejlogo">
-        <img src="zdjecia/jomix-300.png" alt="">
+
+
+    <div class="container-lg centruj rejestracjamain">
+        <div class="logowanielogo rejlogo">
+            <img src="zdjecia/jomix-300.png" alt="">
+        </div>
+
+        <form method="post">
+            <div class="col-auto">
+                <label for="inputImie" class="col-form-label">IMIE</label>
+                <input pattern="[AĄ-ZŹą-z]+" type="text" name="imie" id="inputImie" class="form-control" required>
+
+                <label for="inputNazwisko" class="col-form-label">NAZWISKO</label>
+                <input pattern="[A-Za-z]+" type="text" name="nazwisko" id="inputNazwisko" class="form-control" required>
+
+                <label for="inputEmail" class="col-form-label">EMAIL</label>
+                <input type="text" name="email" id="inputEmail" class="form-control" required>
+
+                <label for="inputHaslo" class="col-form-label">HASŁO</label>
+                <input type="text" name="haslo" id="inputHaslo" class="form-control" required>
+            </div>
+            <div class="logowaniebutton">
+                <br>
+                <input class="purpleBttn" type="submit" name="zarejestrujsie" value="Zarejestruj się">
+            </div>
+
+        </form>
     </div>
 
-    <form method="post">
-        <div class="col-auto">
-            <label for="inputImie" class="col-form-label">IMIE</label>
-            <input type="text" id="inputImie" class="form-control">
+</body>
 
-            <label for="inputNazwisko" class="col-form-label">NAZWISKO</label>
-            <input type="text" id="inputNazwisko" class="form-control">
-
-            <label for="inputEmail" class="col-form-label">EMAIL</label>
-            <input type="text" id="inputEmail" class="form-control">
-
-            <label for="inputHaslo" class="col-form-label">HASŁO</label>
-            <input type="text" id="inputHaslo" class="form-control">
-        </div>
-        <div class="logowaniebutton">
-            <br>
-            <input class="purpleBttn" type="submit" name="zarejestrujsie" value="Zarejestruj się">
-        </div>
-
-    </form>
-</div>
+</html>
