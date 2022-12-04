b2a838d1543c423adeb8f39812bf9aabcf0e144a
KluskaGit <kamil6kluska7@gmail.com>
2022-12-04 14:12:30 +0100

    poprawki_css

--- Produkt.php
+++ Produkt.php
@@ -1,9 +1,10 @@
 <html>
+
 <head>
     <?php
     include 'head.php';
-    include 'laczenieBaza.php'; 
-    
+    include 'laczenieBaza.php';
+
     $produktID = $_GET['produktID'];
     $produkt = mysqli_query($lacz, "SELECT nazwa_produktu, opis_produktu, cena, promocja, sku, img_url from produkty where produktID = $produktID");
     ?>
@@ -15,19 +16,19 @@
     <div class="container-fluid p-0">
         <?php include 'header.php'; ?>
         <div class="container-lg">
-        <?php while ($row=@mysqli_fetch_array($produkt)){
-            echo '<section class="py-5">
+            <?php while ($row = @mysqli_fetch_array($produkt)) {
+                echo '<section class="py-5">
                 <div class=" px-4 px-lg-5 my-5">
                     <div class="row gx-4 gx-lg-5 align-items-center">
-                        <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="'.$row['img_url'].'" /></div>
+                        <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="' . $row['img_url'] . '" /></div>
                         <div class="col-md-6">
                             <div class="small mb-1"></div>
-                            <h1 class="display-5 fw-bolder">'.$row['nazwa_produktu'].'</h1>
+                            <h1 class="display-5 fw-bolder">' . $row['nazwa_produktu'] . '</h1>
                             <div class="fs-5 mb-5">
                                 <span class="text-decoration-line-through"></span>
-                                <span>'.$row['cena'].'</span>
+                                <span>' . $row['cena'] . '</span>
                             </div>
-                            <p class="lead">'.$row['opis_produktu'].'</p>
+                            <p class="lead">' . $row['opis_produktu'] . '</p>
                             <div class="d-flex">
                                 <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" />
                                 <form action="size">
@@ -48,7 +49,7 @@
                 </div>
             </section>';
             }
-        ?>
+            ?>
         </div>
         <?php include 'footer.php'; ?>
     </div>
--- Produkty.php
+++ Produkty.php
@@ -7,32 +7,34 @@
 
 <body>
   <div class="container-fluid p-0">
-    <?php include 'header.php'; 
-    
+    <?php include 'header.php';
+
     $katID = $_GET['katID'];
     $nazwa_kategorii = mysqli_query($lacz, "SELECT nazwa_kategorii from kategorie where kategoriaID=$katID");
     ?>
     <div class="container-lg">
-      <h1><?php while ($x = @mysqli_fetch_array($nazwa_kategorii)) { echo $x['nazwa_kategorii']; } ?></h1>
+      <h1><?php while ($x = @mysqli_fetch_array($nazwa_kategorii)) {
+            echo $x['nazwa_kategorii'];
+          } ?></h1>
 
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 col">
-      <?php
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 col produktybox">
+        <?php
         $produkt = mysqli_query($lacz, "SELECT produktID,nazwa_produktu, cena, img_url, nazwa_kategorii from produkty inner join kategorie on produkty.kategoriaID = kategorie.kategoriaID  where produkty.kategoriaID=$katID");
 
-        while ($row = @mysqli_fetch_array($produkt)){
+        while ($row = @mysqli_fetch_array($produkt)) {
 
-          echo '<div class="col">';
-          echo '<a href="Produkt.php?produktID='.$row['produktID'].'">';
+          echo '<div class="col cardpordukt">';
+          echo '<a href="Produkt.php?produktID=' . $row['produktID'] . '">';
           echo    '<div class="card shadow-sm">';
-          echo      '<img src="'.$row['img_url'].'">';
-          echo      '<h4>'.$row['nazwa_produktu'].'</h4>';
-          echo      '<p>'.$row['nazwa_kategorii'].'</p>';
-          echo      '<h5>'.$row['cena'].'</h5>';
+          echo      '<img src="' . $row['img_url'] . '">';
+          echo      '<h4>' . $row['nazwa_produktu'] . '</h4>';
+          echo      '<p>' . $row['nazwa_kategorii'] . '</p>';
+          echo      '<h5>' . $row['cena'] . '</h5>';
           echo    '</div>';
           echo  '</a>';
           echo '</div>';
-      }
-      ?>
+        }
+        ?>
 
       </div>
     </div>
