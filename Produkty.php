511e68e822634d85b2b0a3d42af46f3b65e61f6e
FiodorowPhotography <fiodo037@gmail.com>
2022-12-04 10:39:41 +0100

    wyświetlanie kategorii

--- Produkty.php
+++ Produkty.php
@@ -7,9 +7,13 @@
 
 <body>
   <div class="container-fluid p-0">
-    <?php include 'header.php'; ?>
+    <?php include 'header.php'; 
+    
+    $katID = $_GET['katID'];
+    $nazwa_kategorii = mysqli_query($lacz, "SELECT nazwa_kategorii from kategorie where kategoriaID=$katID");
+    ?>
     <div class="container-lg">
-      <h1>KOMPLETY</h1>
+      <h1><?php while ($x = @mysqli_fetch_array($nazwa_kategorii)) { echo $x['nazwa_kategorii']; } ?></h1>
 
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 col">
 
@@ -24,60 +28,6 @@
           </a>
         </div>
 
-        <div class="col">
-          <a href="Produkt.php">
-            <div class="card shadow-sm">
-              <img src="zdjecia/produkt.jpg">
-              <h4>Nazwa produktu</h4>
-              <p>Kategoria</p>
-              <h5>79.99zł</h5>
-            </div>
-          </a>
-        </div>
-
-        <div class="col">
-          <a href="Produkt.php">
-            <div class="card shadow-sm">
-              <img src="zdjecia/produkt.jpg">
-              <h4>Nazwa produktu</h4>
-              <p>Kategoria</p>
-              <h5>79.99zł</h5>
-            </div>
-          </a>
-        </div>
-
-        <div class="col">
-          <a href="Produkt.php">
-            <div class="card shadow-sm">
-              <img src="zdjecia/produkt.jpg">
-              <h4>Nazwa produktu</h4>
-              <p>Kategoria</p>
-              <h5>79.99zł</h5>
-            </div>
-          </a>
-        </div>
-
-        <div class="col">
-          <a href="Produkt.php">
-            <div class="card shadow-sm">
-              <img src="zdjecia/produkt.jpg">
-              <h4>Nazwa produktu</h4>
-              <p>Kategoria</p>
-              <h5>79.99zł</h5>
-            </div>
-          </a>
-        </div>
-
-        <div class="col">
-          <a href="Produkt.php">
-            <div class="card shadow-sm">
-              <img src="zdjecia/produkt.jpg">
-              <h4>Nazwa produktu</h4>
-              <p>Kategoria</p>
-              <h5>79.99zł</h5>
-            </div>
-          </a>
-        </div>
 
       </div>
     </div>
