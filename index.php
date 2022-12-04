5b4c971667939a49c4107aa175592ac2343c63d8
KluskaGit <kamil6kluska7@gmail.com>
2022-12-04 12:28:57 +0100

    index_div_close

--- index.php
+++ index.php
@@ -16,28 +16,28 @@
       $nowosci = mysqli_query($lacz, "SELECT produktID,nazwa_produktu, cena, img_url, nazwa_kategorii from produkty inner join kategorie on produkty.kategoriaID = kategorie.kategoriaID LIMIT 4");
       ?>
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 col ">
-      <?php while ($row=@mysqli_fetch_array($nowosci)){
-                  echo '<div class="col">';
-                  echo '<a href="Produkt.php?produktID='.$row['produktID'].'">';
-                  echo    '<div class="card shadow-sm">';
-                  echo      '<img src="'.$row['img_url'].'">';
-                  echo      '<h4>'.$row['nazwa_produktu'].'</h4>';
-                  echo      '<p>'.$row['nazwa_kategorii'].'</p>';
-                  echo      '<h5>'.$row['cena'].'</h5>';
-                  echo    '</div>';
-                  echo  '</a>';
-                  echo '</div>';
-
-      } ?>
-    </div>
-    <br>
-    <div class="absolute">
+        <?php while ($row = @mysqli_fetch_array($nowosci)) {
+          echo '<div class="col">';
+          echo '<a href="Produkt.php?produktID=' . $row['produktID'] . '">';
+          echo    '<div class="card shadow-sm">';
+          echo      '<img src="' . $row['img_url'] . '">';
+          echo      '<h4>' . $row['nazwa_produktu'] . '</h4>';
+          echo      '<p>' . $row['nazwa_kategorii'] . '</p>';
+          echo      '<h5>' . $row['cena'] . '</h5>';
+          echo    '</div>';
+          echo  '</a>';
+          echo '</div>';
+        } ?>
+      </div>
+      <br>
+      <div class="absolute">
         <img src="zdjecia/atuty.jpg" class="img-fluid d-none d-lg-block w-100">
       </div>
       <br>
       <div>
         <img src="zdjecia/baner.jpg" class="img-fluid w-100">
       </div>
+    </div>
     <?php include 'footer.php'; ?>
   </div>
 </body>
