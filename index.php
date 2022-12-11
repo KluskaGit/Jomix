93c5f313145624c9f00fbf6e587dbf1a32ee281f
KluskaGit <kamil6kluska7@gmail.com>
2022-12-11 22:38:26 +0100

    slider

--- index.php
+++ index.php
@@ -9,29 +9,37 @@
   <div class="container-fluid p-0">
     <?php include("header.php"); ?>
 
-    <div id="carouselExampleInterval" class="carousel slide" data-mdb-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
       <div class="carousel-inner">
-        <div class="carousel-item active" data-mdb-interval="10000">
-          <img src="zdjecia/slide1.jpg" class="d-block w-100" />
-        </div>
-        <div class="carousel-item" data-mdb-interval="2000">
-          <img src="zdjecia/slide2.jpg" class="d-block w-100" />
+        <div class="carousel-item active">
+          <img src="zdjecia/slide1.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img src="zdjecia/slide3.jpg" class="d-block w-100" />
+          <img src="zdjecia/slide2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="zdjecia/slide3.jpg" class="d-block w-100" alt="...">
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
 
     <div class="container-lg">
+
+
+      <br>
 
       <h1>NOWOŚCI</h1>
       <?php
