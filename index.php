927acc4eb30ccfbc83b051cbc007ba3d5d443701
FiodorowPhotography <fiodo037@gmail.com>
2022-12-11 22:30:03 +0100

    slider

--- index.php
+++ index.php
@@ -9,6 +9,28 @@
   <div class="container-fluid p-0">
     <?php include("header.php"); ?>
 
+    <div id="carouselExampleInterval" class="carousel slide" data-mdb-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-mdb-interval="10000">
+          <img src="zdjecia/slide1.jpg" class="d-block w-100" />
+        </div>
+        <div class="carousel-item" data-mdb-interval="2000">
+          <img src="zdjecia/slide2.jpg" class="d-block w-100" />
+        </div>
+        <div class="carousel-item">
+          <img src="zdjecia/slide3.jpg" class="d-block w-100" />
+        </div>
+      </div>
+      <button class="carousel-control-prev" data-mdb-target="#carouselExampleInterval" type="button" data-mdb-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Poprzedni</span>
+      </button>
+      <button class="carousel-control-next" data-mdb-target="#carouselExampleInterval" type="button" data-mdb-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Następny</span>
+      </button>
+    </div>
+
     <div class="container-lg">
 
       <h1>NOWOŚCI</h1>
